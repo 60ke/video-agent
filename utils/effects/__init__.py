8beba937a94289eb8b53b9f209368605259dddaa
LookForAdmin <i505125461@live.com>
2026-07-09 16:50:15 +0800

    feat: add effects package init

---
+++ utils/effects/__init__.py
@@ -0,0 +1,21 @@
+from .registry import (
+    EFFECT_NAMES,
+    EFFECTS_REQUIRE_AUX,
+    EffectSuggestionInput,
+    effect_aux_asset_ids,
+    effect_requires_aux,
+    normalize_effect_config,
+    render_effect_frame,
+    suggested_effect,
+)
+
+__all__ = [
+    "EFFECT_NAMES",
+    "EFFECTS_REQUIRE_AUX",
+    "EffectSuggestionInput",
+    "effect_aux_asset_ids",
+    "effect_requires_aux",
+    "normalize_effect_config",
+    "render_effect_frame",
+    "suggested_effect",
+]
