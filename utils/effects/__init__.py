801c9dbe64bd9cb460a18e9cd72630f4432f7ec4
LookForAdmin <i505125461@live.com>
2026-07-10 09:54:31 +0800

    feat: register perspective push-in effect

--- utils/effects/__init__.py
+++ utils/effects/__init__.py
@@ -1,4 +1,9 @@
-from .registry import (
+from . import registry as _registry
+from .perspective_push_in import EFFECT_NAME as PERSPECTIVE_PUSH_IN, install as _install_perspective_push_in
+
+_install_perspective_push_in(_registry)
+
+from .registry import (  # noqa: E402
     EFFECT_NAMES,
     EFFECTS_REQUIRE_AUX,
     EffectSuggestionInput,
@@ -12,6 +17,7 @@ from .registry import (
 __all__ = [
     "EFFECT_NAMES",
     "EFFECTS_REQUIRE_AUX",
+    "PERSPECTIVE_PUSH_IN",
     "EffectSuggestionInput",
     "effect_aux_asset_ids",
     "effect_requires_aux",
