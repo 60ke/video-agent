256c7005d3dd96231ff5e4aa27f3bf4ce65c6324
LookForAdmin <i505125461@live.com>
2026-07-22 17:31:15 +0800

    register AgentTest Remotion composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,5 +1,7 @@
 import { Composition } from "remotion";
+import { AgentTest } from "./AgentTest";
 import { VerticalDemo } from "./VerticalDemo";
+import type { AgentTestProps } from "./AgentTest";
 import type { TimelineProps } from "./types";
 
 const defaults: TimelineProps = {
@@ -12,20 +14,48 @@ const defaults: TimelineProps = {
   subtitles: [],
 };
 
+const agentTestDefaults: AgentTestProps = {
+  width: 1080,
+  height: 1920,
+  fps: 30,
+  frame_count: 30,
+  voice_path: "",
+  title: "Agent Video Test",
+  scenes: [],
+  subtitles: [],
+};
+
 export const Root = () => (
-  <Composition
-    id="VerticalDemo"
-    component={VerticalDemo}
-    defaultProps={defaults}
-    width={1080}
-    height={1920}
-    fps={30}
-    durationInFrames={30}
-    calculateMetadata={({ props }) => ({
-      width: props.width,
-      height: props.height,
-      fps: props.fps,
-      durationInFrames: props.frame_count,
-    })}
-  />
+  <>
+    <Composition
+      id="VerticalDemo"
+      component={VerticalDemo}
+      defaultProps={defaults}
+      width={1080}
+      height={1920}
+      fps={30}
+      durationInFrames={30}
+      calculateMetadata={({ props }) => ({
+        width: props.width,
+        height: props.height,
+        fps: props.fps,
+        durationInFrames: props.frame_count,
+      })}
+    />
+    <Composition
+      id="AgentTest"
+      component={AgentTest}
+      defaultProps={agentTestDefaults}
+      width={1080}
+      height={1920}
+      fps={30}
+      durationInFrames={30}
+      calculateMetadata={({ props }) => ({
+        width: props.width,
+        height: props.height,
+        fps: props.fps,
+        durationInFrames: props.frame_count,
+      })}
+    />
+  </>
 );
